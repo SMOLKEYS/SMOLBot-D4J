8430a9453085599e3af1e99adfc79646e6b253d8
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-11-17 01:03:45 +0800

    devious beings

--- app/src/main/java/smol/Loader.java
+++ app/src/main/java/smol/Loader.java
@@ -13,7 +13,7 @@ public class Loader{
     public static void beginClient(GatewayDiscordClient client){
         Seq<String> heWait = Seq.with("veloren", "with no commands :pain:", "with smolkeys", "maid", "with a sleep session", "help me", "ahahahahahahahhahaha");
         
-        SmolBot.client.gateway().setInitialPresence(aspac -> ClientPresence.doNotDisturb(ClientActivity.playing(heWait.random())));
+        Time.run(60f * 20f, () -> SmolBot.client.gateway().setInitialPresence(aspac -> ClientPresence.doNotDisturb(ClientActivity.playing(heWait.random()))));
         
         Time.run(60f * 14400f, () -> client.logout());
     }
