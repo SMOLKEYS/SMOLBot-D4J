b2adf4aa4c3f3064a129335fe3691cac99113ff0
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-11-16 17:51:46 +0800

    mfw no return

--- app/src/main/java/smol/commands/Commands.java
+++ app/src/main/java/smol/commands/Commands.java
@@ -5,6 +5,7 @@ import arc.struct.*;
 import discord4j.core.*;
 import discord4j.core.event.domain.message.*;
 import discord4j.core.object.entity.*;
+import reactor.core.publisher.*;
 
 public class Commands{
     private static ObjectMap<String, Cons<Message>> stringCommands = new ObjectMap<String, Cons<Message>>();
@@ -13,6 +14,8 @@ public class Commands{
     public static void listenerBegin(GatewayDiscordClient client){
         client.on(MessageCreateEvent.class, msg -> {
             if(stringCommands.containsKey(msg.getMessage().getContent())) stringCommands.get(msg.getMessage().getContent()).get(msg.getMessage());
+            
+            return Mono.empty();
         });
     }
     
