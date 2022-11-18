c126de82d7201963dc50ef90db03535e83d058ba
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-11-18 22:59:34 +0800

    mood

--- app/src/main/kotlin/smol/util/Utils.kt
+++ app/src/main/kotlin/smol/util/Utils.kt
@@ -6,4 +6,6 @@ import dev.kord.core.entity.channel.*
 
 suspend fun Kord.getTextChannel(id: Snowflake) = this.getChannel(id) as TextChannel
 
+
+fun Channel.toTextChannel() = this as TextChannel
 fun ULong.toSnowflake() = Snowflake(this)
