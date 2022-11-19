7985aaeba807a1fe9292ba98f5948e7f74e8c253
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-11-19 23:38:49 +0800

    a

--- app/src/main/kotlin/smol/util/Utils.kt
+++ app/src/main/kotlin/smol/util/Utils.kt
@@ -5,7 +5,7 @@ import dev.kord.core.entity.*
 import dev.kord.common.entity.*
 import dev.kord.core.entity.channel.*
 import dev.kord.core.behavior.reply
-
+import kotlinx.coroutines.*
 
 suspend fun Kord.getTextChannel(id: Snowflake) = this.getChannel(id) as TextChannel
 
@@ -31,3 +31,8 @@ fun String.enforce(sub: Int = 0) = this.take(2000 - sub)
 fun String.blockWrap(): String{
     return "```\n${this.enforce(8)}\n```"
 }
+
+
+inline fun launch(crossinline l: suspend CoroutineScope.() -> Unit) = Vars.client.launch{ l() };
+
+inline fun <R> async(crossinline l: suspend CoroutineScope.() -> R) = Vars.client.async{ l() };
