816ef79ed2dfdcaa3f32270c78dc189c2355718b
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-12-01 01:10:29 +0800

    mmh

--- app/src/main/kotlin/smol/commands/Commands.kt
+++ app/src/main/kotlin/smol/commands/Commands.kt
@@ -333,7 +333,7 @@ object Commands{
                     Vars.battle.begin(first, second, if(health > 15000) 15000 else health).forEach{
                         delay(1000 * 2L)
                         ms.edit{
-                            val upcoming = "${ms.content}\n$it"
+                            val upcoming = "${ms.fetchMessage().content}\n$it"
                             
                             content = if(upcoming.length > 355) upcoming.substring(upcoming.length - 355) else upcoming
                         }
