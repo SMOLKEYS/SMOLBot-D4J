75e193c901fbc188dd471d19a8b70f96f72d2624
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-12-01 00:25:20 +0800

    eh

--- app/src/main/kotlin/smol/Vars.kt
+++ app/src/main/kotlin/smol/Vars.kt
@@ -60,14 +60,14 @@ object Vars{
 	}
 	
 	fun load(){
-	    battle.addWeapon("{0} punched {1}!", 1..3)
-	    battle.addWeapon("Lightning called down by {0} hit {1}!", 40..100)
-	    battle.addWeapon("Fist storm from {0}! All fists landed on {1}!", 10..60)
-	    battle.addWeapon("{0} sent burning coal to {1}!", 15..20)
-	    battle.addWeapon("{0} kicked {1}!", 4..9)
-	    battle.addWeapon("Storming flames!\n{0} called a firestorm onto {1}!", 300..550)
-	    battle.addWeapon("{0} dropped a piano onto {1}!", 150..650)
-	    battle.addWeapon("{0} became based! {1} couldn't handle it and lost a lot of health!", 250..1000)
-	    battle.addWeapon("{0}, {0}! YOU'VE MADE A DEAL WITH ME, ME!\n{1} was hit by a scythe!", 860..870)
+	    CombatCommand.addWeapon("{0} punched {1}!", 1..3)
+	    CombatCommand.addWeapon("Lightning called down by {0} hit {1}!", 40..100)
+	    CombatCommand.addWeapon("Fist storm from {0}! All fists landed on {1}!", 10..60)
+	    CombatCommand.addWeapon("{0} sent burning coal to {1}!", 15..20)
+	    CombatCommand.addWeapon("{0} kicked {1}!", 4..9)
+	    CombatCommand.addWeapon("Storming flames!\n{0} called a firestorm onto {1}!", 300..550)
+	    CombatCommand.addWeapon("{0} dropped a piano onto {1}!", 150..650)
+	    CombatCommand.addWeapon("{0} became based! {1} couldn't handle it and lost a lot of health!", 250..1000)
+	    CombatCommand.addWeapon("{0}, {0}! YOU'VE MADE A DEAL WITH ME, ME!\n{1} was hit by a scythe!", 860..870)
 	}
 }
