4e60edebf92f7a4458a406d03a0839786dc02222
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-12-03 21:51:56 +0800

    da sh

--- app/src/main/kotlin/smol/Vars.kt
+++ app/src/main/kotlin/smol/Vars.kt
@@ -79,6 +79,8 @@ object Vars{
         CombatCommand.addWeapon("{0} swung a bat onto {1}'s head!", 15..25)
         CombatCommand.addWeapon("{0} bombed {1}!", 25..45)
         
+        jsScriptEngine = RhinoEngine()
+        
         jsScriptEngine.eval(resourceAsString("/scripts/global.js")!!)
     }
 }
